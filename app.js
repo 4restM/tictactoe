d820d8e17196e2214af3cb3e70525fd4ae09f7af
Forrest Miller <forrest.w.miller@icloud.com>
2017-03-06 16:31:23 -0600

    initialized app, and began iterating on app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk')
+
+console.log(chalk.cyan("Lets Play A Game"))
+
+console.log(chalk.yellow.inverse('here is a reference sheet for board positions'))
+
+console.log(chalk.magenta([7], "|",[8], "|",[9]));
+console.log(chalk.magenta([4], "|",[5], "|",[6]));
+console.log(chalk.magenta([1],"|",[2],"|",[3]));
+
+console.log(chalk.yellow('Use these positions when adding your token'))
+
