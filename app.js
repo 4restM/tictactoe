0e3446bd2494e900f490e39d7335c68ea0692c17
Forrest Miller <forrest.w.miller@icloud.com>
2017-03-06 17:17:28 -0600

    finished render board function

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const chalk = require('chalk')
 
 console.log(chalk.cyan("Lets Play A Game"))
 
-console.log(chalk.yellow.inverse('here is a reference sheet for board positions'))
+// console.log(chalk.yellow.inverse('here is a reference sheet for board positions'))
 
-console.log(chalk.magenta([7], "|",[8], "|",[9]));
-console.log(chalk.magenta([4], "|",[5], "|",[6]));
-console.log(chalk.magenta([1],"|",[2],"|",[3]));
+//flesh this out more
+// console.log(chalk.magenta([7], "|",[8], "|",[9]));
+// console.log(chalk.magenta([4], "|",[5], "|",[6]));
+// console.log(chalk.magenta([1],"|",[2],"|",[3]));
 
-console.log(chalk.yellow('Use these positions when adding your token'))
+// console.log(chalk.yellow('Use these positions when adding your token'))
 
 // build game board function
 const board = [];
@@ -21,12 +22,31 @@ const createBoard = () => {
   }
 }
 createBoard();
-console.log(board)
 
-// build toggle piece function
+// build Render Board Function
+
+const renderBoard = () => {
+  board.forEach(item => console.log(chalk.magenta(item)))
+}
+renderBoard();
+// build toggle player piece function
+  //get piece Position then check if position is occupied 
+    // if not then place a X there
+  //render board  
+  //check if winner
+    // if not invoke dummy AI
+
+// build toggle AI piece function
+  //get piece Position through Math.random() 
+  //then check if position is occupied 
+    // if not then place a X there
+  //render board
+  //check if winner
 
 // build check piece function
-
-// build dummy AI to randomly place tickets
+  //recursively check on its self
 
 // build winning or draw checker function
+  //If 3 O in a row, col, diag then player won
+  //if 3 X in a row, col, diag then AI won
+  //reset board
