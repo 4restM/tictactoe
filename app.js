04425a7127278af04bc0d25992caa35a2803a34b
Forrest Miller <forrest.w.miller@icloud.com>
2017-03-06 17:11:13 -0600

    pseudocoding and fleshing it out

--- app.js
+++ app.js
@@ -10,3 +10,23 @@ console.log(chalk.magenta([1],"|",[2],"|",[3]));
 
 console.log(chalk.yellow('Use these positions when adding your token'))
 
+// build game board function
+const board = [];
+const createBoard = () => {
+  for(let i = 0; i < 3; i ++){
+    board.push([]);
+    for(let j = 0; j < 3; j++){
+      board[i].push('-')
+    }
+  }
+}
+createBoard();
+console.log(board)
+
+// build toggle piece function
+
+// build check piece function
+
+// build dummy AI to randomly place tickets
+
+// build winning or draw checker function
